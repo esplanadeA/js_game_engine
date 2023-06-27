5273257984d9ba7436dcd1147543a9ed9207853b
esplanadeA <anna.li.dev@gmail.com>
2023-06-27 23:19:14 +1200

    js file updated with file links correct

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -7,7 +7,7 @@
 //3. call the function: and loop it
 
 // For head part
-console.log('Dress The Clown!')
+console.log('Dress The Clown!');
 //(counting times function called)
 // let clothingIndex = {
 //   head: 0,
@@ -22,50 +22,50 @@ console.log('Dress The Clown!')
 // take the array of elements with"head,body,shoes"
 //loop them adn qur
 
-let head = document.getElementById('head')
-let body = document.getElementById('body')
-let shoes = document.getElementById('shoes')
+let head = document.getElementById('head');
+let body = document.getElementById('body');
+let shoes = document.getElementById('shoes');
 
-let bodyVars = [body, head, shoes]
-let bodyParts = ['head', 'body', 'shoes']
-let bodyIndex = 0
-let bodyArray = [0, 0, 0]
+let bodyVars = [body, head, shoes];
+let bodyParts = ['head', 'body', 'shoes'];
+let bodyIndex = 0;
+let bodyArray = [0, 0, 0];
 // console.log(bodyVars)
 // for (let i = 0; i < bodys.length; i++)
 
-document.onkeydown = checkKey
+document.onkeydown = checkKey;
 
 function checkKey(e) {
   if (e.key === 'ArrowUp') {
-    changeBodyPart(-1)
+    changeBodyPart(-1);
   } else if (e.key === 'ArrowDown') {
-    changeBodyPart(1)
+    changeBodyPart(1);
   } else if (e.key === 'ArrowLeft') {
-    changeClownHead(-1)
+    changeClownHead(-1);
   } else if (e.key === 'ArrowRight') {
-    changeClownHead(1)
+    changeClownHead(1);
   }
 }
 
 function changeBodyPart(shift) {
-  bodyIndex += shift
-  if (bodyIndex < 0) bodyIndex = 2
-  if (bodyIndex > 2) bodyIndex = 0
+  bodyIndex += shift;
+  if (bodyIndex < 0) bodyIndex = 2;
+  if (bodyIndex > 2) bodyIndex = 0;
 }
 
 // use the right arrow key to move between different head images.
 // document.addEventListener(''))
 function changeClownHead(shift) {
-  bodyArray[bodyIndex] += shift
+  bodyArray[bodyIndex] += shift;
 
   if (bodyArray[bodyIndex] < 0) {
-    bodyArray[bodyIndex] = 5
+    bodyArray[bodyIndex] = 5;
   }
   if (bodyArray[bodyIndex] > 5) {
-    bodyArray[bodyIndex] = 0
+    bodyArray[bodyIndex] = 0;
   }
-  let bodyPart = bodyParts[bodyIndex]
-  let imgIndex = bodyArray[bodyIndex]
-  bodyVars[bodyIndex].src = `./images/${bodyPart}${imgIndex}.png`
-  console.log(bodyArray[bodyIndex])
+  let bodyPart = bodyParts[bodyIndex];
+  let imgIndex = bodyArray[bodyIndex];
+  bodyVars[bodyIndex].src = `./images/${bodyPart}${imgIndex}.png`;
+  console.log(bodyArray[bodyIndex]);
 }
