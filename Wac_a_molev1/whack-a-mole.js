82b5ee39bfc25a30cd1475812d32f780f32ef81c
esplanadeA <anna.li.dev@gmail.com>
2023-07-01 20:57:57 +1200

    button adding score to the page

--- Wac_a_molev1/whack-a-mole.js
+++ Wac_a_molev1/whack-a-mole.js
@@ -1,7 +1,3 @@
-// -    -   -   -   -  //
-// JAVASCRIPT CARNIVAL //
-// -    -   -   -   -  //
-// The ability to store or get references to the cells in the HTML table.
 //step 1 select the cell and put in the array
 //2.make it random
 //3. put the image in
@@ -29,34 +25,66 @@ function randomCell(arr) {
 // 1.adding img tag
 const image = document.createElement('img');
 // adding image detail
-image.src = './mole.PNG';
+image.src = './mole.png';
 image.id = 'mole';
 image.width = '75';
 image.height = '75';
+
+let score = 0;
+const scoreElement = document.getElementById('WAM_score');
+let isIntervalPaused = false;
+
 // put the image to the random cell
 function showImageInRandomCell() {
+  if (isIntervalPaused) {
+    return;
+  }
   randomCell(cellCollection).appendChild(image);
+  let object = document.getElementById('mole');
+  console.log(object);
+  object.addEventListener('click', whackedMole);
 }
 // click button to start the game
 const button = document.getElementById('myButton');
 button.addEventListener('click', function () {
   showImageInRandomCell(); // Call the function on button click
-  intervalId = setInterval(showImageInRandomCell, 500); // Start the interval
+  intervalId = setInterval(showImageInRandomCell, 800); // Start the interval
   setTimeout(function () {
     clearInterval(intervalId); // Stop the interval after 10 seconds
   }, 20000);
 });
 
-let object = document.getElementById('mole');
-object.addEventListener('click', whackedMole);
-
 function whackedMole() {
-  randomCell(cellCollection);
-  //   call the function again once clicked
+  // Change the source of the mole image
+  image.src = './got_a_mole.png';
   randomCell(cellCollection).appendChild(image);
+  isIntervalPaused = true;
+  setTimeout(function () {
+    isIntervalPaused = false;
+  }, 1000);
+
   var audio = new Audio();
   audio.src = './whack-audio.wav';
   audio.play();
+
+  if (image.src.endsWith('mole.png')) {
+    // Increase the score
+    score++;
+    // Update the score in the HTML
+    scoreElement.textContent = 'Score:' + score;
+  }
+
+  setTimeout(function () {
+    image.src = './mole.png';
+    randomCell(cellCollection).appendChild(image);
+  }, 1000);
 }
 
 // Function to stop the interval after 10 seconds
+// function to record score
+// function score (){
+// get the id for the WAM_score
+// push the score back
+// if mole image is clicked
+// return score ++
+// if mole is not clicked return nothing
