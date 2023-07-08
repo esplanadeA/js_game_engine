95329230b4e9b960c8b07272384fc7f1668ed1c9
esplanadeA <anna.li.dev@gmail.com>
2023-07-08 23:39:38 +1200

    responsive media query added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
   content: ['./*.{html,js}', './Wac_a_molev1/*.{html.js}'],
   theme: {
     screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      desktop: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      laptop: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      tablet: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
     },
     fontFamily: {
       display: ['Gilroy', 'sans-serif'],
