2d73ef22f96641bcf3a3d1f81d77f21c6f826f9f
esplanadeA <anna.li.dev@gmail.com>
2024-03-12 12:35:30 +1300

    logic fix for flip card, sound effect add to function calls

--- memorygame/app.js
+++ memorygame/app.js
@@ -76,6 +76,23 @@ document.addEventListener('DOMContentLoaded', () => {
   let cardChosenIds = [];
   let cardsWon = [];
 
+  function clickSoundPlay() {
+    const clickSound = new Audio('sounds/click_pop.mp3');
+    clickSound.play();
+    //  const winningSound = new Audio('sounds/winning.mp3');
+    //  const failSound = new Audio('sounds/lose.mp3');
+  }
+  function winningSoundPlay() {
+    const winningSound = new Audio('sounds/winning.mp3');
+    winningSound.play();
+
+    //  const failSound = new Audio('sounds/lose.mp3');
+  }
+  function wrongSoundPlay() {
+    const failSound = new Audio('sounds/wrong.wav');
+    failSound.play();
+  }
+
   // create image board
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
@@ -84,35 +101,54 @@ document.addEventListener('DOMContentLoaded', () => {
       card.setAttribute('data-id', i);
       card.setAttribute('class', 'RM_img');
       card.addEventListener('click', flipCard);
+      card.addEventListener('click', clickSoundPlay);
       gridDisplay.appendChild(card);
     }
   }
 
   function checkForMatch() {
-    const cards = document.querySelectorAll('img');
+    const cards = document.querySelectorAll('.RM_img');
     console.log(cards);
-    if (cardChosenIds[0] == cardChosenIds[1]) {
+    console.log(cardChosenIds);
+    console.log(cardsWon);
+
+    const card1 = cards[cardChosenIds[0]];
+    const card2 = cards[cardChosenIds[1]];
+
+    if (cardChosenIds[0] === cardChosenIds[1]) {
       cards[cardChosenIds[0]].setAttribute('src', 'images/blank.png');
       cards[cardChosenIds[1]].setAttribute('src', 'images/blank.png');
       alert('You have clicked the same image!');
     } else if (cardsChosen[0] === cardsChosen[1]) {
+      winningSoundPlay();
       alert('you find a match');
       console.log(cardChosenIds[0]);
-      cards[cardChosenIds[0]].setAttribute('src', 'images/clicked.png');
-      cards[cardChosenIds[1]].setAttribute('src', 'images/clicked.png');
-      cards[cardChosenIds[0]].removeEventListener('click', flipCard);
-      cards[cardChosenIds[1]].removeEventListener('click', flipCard);
-      cardsWon.push[cardChosen];
+      card1.setAttribute('src', 'images/clicked.png');
+      card2.setAttribute('src', 'images/clicked.png');
+      // Optionally remove event listeners if needed
+      card1.removeEventListener('click', flipCard);
+      card2.removeEventListener('click', flipCard);
+      cardsWon.push(cardChosenIds[0]);
+      cardsWon.push(cardChosenIds[1]);
+      cardsChosen = [];
+      cardChosenIds = [];
     } else {
-      cards[cardChosenIds[0]].setAttribute('src', 'images/blank.png');
-      cards[cardChosenIds[1]].setAttribute('src', 'images/blank.png');
+      cardsChosen = [];
+      cardChosenIds = [];
+      wrongSoundPlay();
       alert('Sorry try again');
+      card1.setAttribute('src', 'images/blank.png');
+      card2.setAttribute('src', 'images/blank.png');
+      // card1.removeEventListener('click', flipCard);
+      // card2.removeEventListener('click', flipCard);
     }
-    cardsChosen = [];
-    cardChosenIds = [];
+    //  cardsChosen = [];
+    //  cardChosenIds = [];
+    //  cardChosenIds = [];
 
-    resultDisplay.textContent = cardsWon.length;
-    if (cardsWon.length === cardArray.length / 2) {
+    //  resultDisplay.textContent = cardsWon.length;
+    console.log(cardsWon);
+    if (cardsWon.length === cardArray.length) {
       resultDisplay.textContent = 'Congratulations! You found them all!';
     }
   }
@@ -151,8 +187,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function flipCard() {
     const cardId = this.getAttribute('data-id');
-
     cardsChosen.push(cardArray[cardId].name);
+    console.log(cardsChosen);
+    console.log(cardChosenIds);
     cardChosenIds.push(cardId);
     this.setAttribute('src', cardArray[cardId].img);
     if (cardsChosen.length === 2) {
