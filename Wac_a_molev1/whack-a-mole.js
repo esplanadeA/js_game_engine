e73cec31d63de85e5d9fdb219b0c0c7830effffa
esplanadeA <anna.li.dev@gmail.com>
2024-03-11 20:41:20 +1300

    image update function ademend for show image after click mole

--- Wac_a_molev1/whack-a-mole.js
+++ Wac_a_molev1/whack-a-mole.js
@@ -1,12 +1,12 @@
 // get the table element to an array
 const cellCollection = Array.from(document.querySelectorAll('td'));
+console.log(cellCollection);
 
 // randomly select a cell from the array(an random index)
 function randomCell(arr) {
   const randomIndex = Math.floor(Math.random() * arr.length);
 
   const item = arr[randomIndex];
-
   return item;
 }
 
@@ -18,7 +18,6 @@ image.src = './mole.png';
 image.id = 'mole';
 image.width = '75';
 image.height = '75';
-
 let score = 0;
 const scoreElement = document.getElementById('WAM_score');
 let isIntervalPaused = false;
@@ -27,7 +26,11 @@ let isIntervalPaused = false;
 let imageDisplayCount = 0;
 // Maximum number of image displays
 const maxImageDisplays = 10;
+let intervalId;
 
+function startGameInterval() {
+  intervalId = setInterval(showImageInRandomCell, 800);
+}
 // put the image to the random cell
 function showImageInRandomCell() {
   if (isIntervalPaused) {
@@ -40,24 +43,27 @@ function showImageInRandomCell() {
   imageDisplayCount++;
   if (imageDisplayCount >= maxImageDisplays) {
     // Stop the interval after reaching the maximum image displays
-
     clearInterval(intervalId);
+    const gameOverMessage = document.getElementById('gameOverMessage');
+    gameOverMessage.textContent = 'Game Over';
+    gameOverMessage.style.display = 'block';
   }
 }
 // click button to start the game
 const button = document.getElementById('myButton');
 button.addEventListener('click', function () {
-  showImageInRandomCell(); // Call the function on button click
-  intervalId = setInterval(showImageInRandomCell, 800); // Start the interval
+  showImageInRandomCell();
+  startGameInterval(); // Call the function on button click
+  // Start the interval
   setTimeout(function () {
     clearInterval(intervalId); // Stop the interval after 10 seconds
   }, 20000);
 });
 
-function whackedMole() {
+function whackedMole(event) {
   // Change the source of the mole image
-  image.src = './got_a_mole.png';
-  randomCell(cellCollection).appendChild(image);
+  const clickedMole = event.target; // Get the clicked mole image element
+  clickedMole.src = './got_a_mole.png';
   isIntervalPaused = true;
   setTimeout(function () {
     isIntervalPaused = false;
@@ -67,7 +73,7 @@ function whackedMole() {
   audio.src = './whack-audio.wav';
   audio.play();
 
-  if (image.src.endsWith('mole.png')) {
+  if (clickedMole.src.endsWith('mole.png')) {
     // Increase the score
     score++;
     // Update the score in the HTML
@@ -76,7 +82,7 @@ function whackedMole() {
 
   if (imageDisplayCount >= maxImageDisplays) {
     object.removeEventListener('click', whackedMole); // Remove the click event listener
-    return (image.src = './end.png'); // Stop further image displays
+    return (clickedMole.src = './end.png'); // Stop further image displays
   }
 
   setTimeout(function () {
